87268dfd27221e22fa699430656bef7ff0728ba0
Intan Atsilah <intanatsila578@gmail.com>
2023-10-24 20:19:52 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package modul_3;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("GitHub i'm comin'");
+    }
+}
